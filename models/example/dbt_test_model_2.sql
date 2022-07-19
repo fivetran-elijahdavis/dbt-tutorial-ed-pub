1f7afcd13e64d46cee80cda692f054160365d5df
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2022-07-19 09:15:11 -0600

    Update dbt_test_model_2.sql

--- models/example/dbt_test_model_2.sql
+++ models/example/dbt_test_model_2.sql
@@ -1,3 +1,3 @@
 
 -- Use the `ref` function to select from other models
-{{ salesforce_formula_utils.sfdc_formula_view(source_table='FIVETRAN_FORMULA') }}
+{{ salesforce_formula_utils.sfdc_formula_view(source_table="ELIJAH_DB"."ELIJAH_SALESFORCE_SANDBOX"."FIVETRAN_FORMULA") }}
